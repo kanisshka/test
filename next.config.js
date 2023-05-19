f8462d48644df4e4d78984024c50555bc11236ad
scrunchiefreaks <122561845+scrunchiefreaks@users.noreply.github.com>
2023-05-19 20:05:12 +0530

    a

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  ignoreDuringBuilds: true,
   swcMinify: true,
   images: {
     domains: ['img.freepik.com','fakestoreapi.com'],
